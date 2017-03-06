639218bb356a998d5c65c796737ef9e9c681894c
Zach Taylor <zach@planetjester.com>
2017-03-05 20:04:36 -0500

    Fixed Library

---
+++ EngineLib/EngineLib/MapData.cs
@@ -0,0 +1,31 @@
+﻿namespace EngineLib
+{
+    class MapData
+    {
+        public TileInfo[] Data;
+
+        public int Width { get; set; }
+
+        public int Height { get; set; }
+
+        public int SquareHeight { get; set; }
+
+        public int SquareWidth { get; set; }
+
+        public MapData(int width, int height, int sqht, int sqwd)
+        {
+            Width = width;
+            Height = height;
+            SquareHeight = sqht;
+            SquareWidth = sqwd;
+            var rows = Height / sqht;
+            var columns = Width / sqwd;
+            var cells = rows * columns;
+            Data = new TileInfo[cells];
+        }
+
+        public MapData()
+        {
+        }
+    }
+}
---
+++ EngineLib/EngineLib/TileInfo.cs
@@ -0,0 +1,28 @@
+﻿namespace EngineLib
+{
+    class TileInfo
+    {
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        public int Id { get; set; }
+
+        public int Xmodifier { get; set; }
+
+        public int Ymodifier { get; set; }
+
+        public TileInfo(int xcoord, int ycoord, int colorId, int xmod, int ymod)
+        {
+            X = xcoord;
+            Y = ycoord;
+            Id = colorId;
+            Xmodifier = xmod;
+            Ymodifier = ymod;
+        }
+
+        public TileInfo()
+        {
+        }
+    }
+}
